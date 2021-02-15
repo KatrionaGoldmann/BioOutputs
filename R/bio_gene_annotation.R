8015159c5ebb23b94cde145445e4994f1eb57b28
EMR <katrionagoldmann@gmail.com>
2021-02-15 12:15:09 +0000

    Add enrichr analysis function

--- R/bio_gene_annotation.R
+++ R/bio_gene_annotation.R
@@ -3,7 +3,7 @@ library(easyPubMed)
 library(rentrez)
 library(httr)
 library(pbapply)
-
+library(enrichR)
 
 # wrapper function to find all info about a list of genes
 #' @param genes
@@ -19,12 +19,13 @@ library(pbapply)
 #' @param publication_keywords Words to search for publications with the genes 
 #' @param publication_split Whether to use AND or OR when searching the 
 #' publication keywords (default is OR)
-what_do_you_do <- function(genes, 
+gene_summary <- function(genes, 
                  verbose=TRUE, 
                  gene_types=TRUE, 
                  gene_summaries=TRUE, 
                  associated_diseases=TRUE, 
-                 publications=FALSE, 
+                 publications=FALSE,
+                 gene_types_list = immune_types,
                  disease_cutoff=0, 
                  diseases=c("C20", "C05", "C10", "C17"), 
                  publication_keywords=c("Rheuamtoid", "Autoimmune"), 
@@ -33,7 +34,7 @@ what_do_you_do <- function(genes,
   
   if(gene_types){
     print("Annotating EMR's favourites...")
-    df$Type <- what_are_you(genes, c(immune_types, Others))$Type
+    df$Type <- what_are_you(genes, gene_types_list)$Type
   }
   
   if(gene_summaries){
@@ -66,54 +67,102 @@ what_do_you_do <- function(genes,
   return(df)
 }
 
+#' returns pathways from enrichR
+#' @param genes List of gene names
+#' @param drop_terms terms in databases not to search. If NULL only keep_terms 
+#' used
+#' @param keep_terms terms in databases to search. If NULL only all except 
+#' drop_terms used
+#' @param dbs database of enrichR pathways. If NULL all are selected minus 
+#' those removed by drop_terms. See listEnrichrDbs().  
+#' @param cutoff pvalue cutoff
+#' @param min_N minimum number of genes to consider
+enriched_pathways <- function(genes, 
+                              drop_terms=c("Genome_Browser", "GTEx", "Grants", 
+                                           "Funded", "SysMyo", "Pfam", "CORUM", 
+                                           "LINCS", "User"), 
+                              keep_terms=c("KEGG", "Reactome", "DisGeNET", 
+                                          "Elsevier", "GWAS"),
+                              dbs=NULL, 
+                              cutoff=0.05, 
+                              min_N=2){
+  
+  if(is.null(dbs)) dbs <- listEnrichrDbs() 
+  if(! is.null(drop_terms)) {
+    dbs <- dbs[! grepl(paste0(drop_terms, collapse="|"), dbs$libraryName), ]
+  }
+  if(! is.null(drop_terms)) {
+    dbs <- dbs[grepl(paste0(keep_terms, collapse="|"), dbs$libraryName), ]
+  }
+
+  enriched <- enrichr(genes, dbs$libraryName)
+  
+  temp = do.call(rbind, enriched)
+  
+  temp$N = as.numeric(gsub("/.*", "", temp$Overlap))
+  temp = temp[temp$N >= min_N, ]
+  temp = temp[temp$P.value <= cutoff, ]
+  
+  temp = temp[order(temp$P.value), ]
+  
+  temp_genes = sort(table(unlist(strsplit(temp$Genes,  ";"))), decreasing=TRUE)
+  return(list(enrichment=temp, enrichment_genes=temp_genes))
+}
+
 # Immune type genes EMR are interested in 
-immune_types = list("Collagens"=c("COL", ""),
-                    "HOX"=c("HOX", ""),
-                    "Cluster of Differentiation"=c("^CD[0-9]", ""),
-                    "Interleukins and their receptors"=c("^IL[0-9]", ""),
+immune_types = list("ADAM"=c("ADAM", ""),
+                    "ALDOA" =c("ALDO", ""),
+                    "BMP"=c("BMP", ""),
+                    "CAMK"=c("CAMK", ""),
+                    "CIDE"=c("CIDE", ""),
                     "Chemokines"=c("CCL|CXC", "signalling & communication"),
+                    "Cluster of Differentiation"=c("^CD[0-9]", ""),
+                    "Collagens"=c("^COL", ""),
                     "Complement"=c('^C[0-9]', ""),
                     "Complement Receptors"=c('^CR[0-9]', ""),
-                    "Interferon (IFI)" = c("IFI", ""),
+                    "CREB"=c("CRE", ""),
+                    "Dual-specificity phosphatase"=c("^DUSP", ""),
+                    "Fibroblast Growth Factors"=c("FGF", ""),
+                    "FLG"=c("FLG", ""),
+                    "FOS"=c("FOS", "regulators of cell proliferation, differentiation, and transformation"),
+                    "GAS"=c("GAS", ""),
+                    "HLA"=c("HLA", ""),
+                    "HOX"=c("HOX", ""),
+                    "Immunoglobulins (heavy)"=c("^IGH", "Antibodies"),
+                    "Immunoglobulins (lamda)"=c("^IGLV", "Antibodies"),
+                    "Immunoglobulins (kappa)"=c("^IGK", "Antibodies"),
+                    "Interferon induced transmembrane proteins" = c("IFITM", ""),
                     "Interferon (OAS)" = c("OAS", ""),
                     "Interferon (ISG)" = c("ISG", ""),
-                    "Interferon (IRF)" = c("IRF", ""),
+                    "Interferon regulatory factors" = c("IRF", ""),
                     "Interferon (IFN)" = c("IFN", ""),
-                    "TLR"=c("TLR", ""),
-                    "ADAM"=c("ADAM", ""),
-                    "STAT"=c("STAT", ""),
-                    "GAS"=c("GAS", ""),
-                    "Fibroblast Growth Factors"=c("FGF", ""),
-                    "CAMK"=c("CAMK", ""),
-                    "TRAF"=c("TRAF", ""),
-                    "TNF"=c("TNF", ""),
-                    "BMP"=c("BMP", ""),
-                    "WNT"=c('WNT', ""),
-                    "MS4A"=c("MS4A", ""),
+                    "Interleukins and their receptors"=c("^IL[0-9]", ""),
                     "KRT"=c("KRT", ""),
-                    "NLRP"=c("NLRP", ""),
-                    "PSORS"=c("PSO", ""),
-                    "NFK"=c("NFK", ""),
-                    "ALDOA" =c("ALDO", ""),
-                    "CIDE"=c("CIDE", ""),
-                    "FLG"=c("FLG", ""),
-                    "TP5"=c("^TP5", ""),
+                    "Killer Cells"=c("KLR", "Natural killer (NK) cells are lymphocytes that can mediate lysis of certain tumor cells and virus-infected cells without previous activation"),
+                    "MAPK"=c("^MAP[0-9]|MAPK", "kinase kinases: signalling, communication"),
                     "MMP"=c("MMP", ""),
+                    "MS4A"=c("MS4A", ""),
+                    "NAD(P)H:quinone acceptor oxidoreductase"=c("^NQO", ""),
+                    "NFK"=c("NFK", ""),
+                    "NLRP"=c("NLRP", ""),
                     "NOTCH"=c("NOTCH", ""),
-                    "USP"=c("USP", ""),
-                    "CREB"=c("CRE", ""),
-                    "MAPK"=c("MAPK", "kinase kinases: signalling, communication"),
-                    "HLA"=c("HLA", ""),
-                    "SAA"=c("SAA", ""),
                     "Phosphorlation Genes"=c("PHOSPHO", "Protein phosphorylation. Hydrolase superfamily"),
-                    "FOS"=c("FOS", "regulators of cell proliferation, differentiation, and transformation"),
+                    "Phosphatase"=c("^PTP", "critical regulators of signal transduction"),
+                    "PSORS"=c("PSO", ""),
                     "RAS"=c("RAS|RALA", "GTPases which act as molecular switches in the cell"),
                     "RGS"=c("RGS", "Pain signalling"),
-                    "Immunoglobulins (IGH)"=c("^IGH", "Antibodies"),
-                    "Immunoglobulins (IGL)"=c("^IGLV", "Antibodies"),
-                    "Immunoglobulins (IGK)"=c("^IGK", "Antibodies"))
+                    "S100"=c("^S100", "regulation of protein phosphorylation, transcription factors, Ca2+ homeostasis, the dynamics of cytoskeleton constituents, enzyme activities, cell growth and differentiation, and the inflammatory response"),
+                    "SAA"=c("^SAA", ""),
+                    "STAT"=c("^STAT", ""),
+                    "TLR"=c("TLR", ""),
+                    "TNF"=c("TNF", ""),
+                    "TP5"=c("^TP5", ""),
+                    "TRAF"=c("TRAF", ""),
+                    "Ubiquitin specific peptidases"=c("^USP", ""),
+                    "Ubiquitin Family"=c("^UBF", ""),
+                    "WNT"=c('WNT', ""))
 
-Others = list("Other noteworthy genes"=c("GSK3|HYAL|UBF|KLF|GATA|PFKFB|ACAN|S100B", ""))
+
 
 #' genes EMR are interested in
 #' @param genes A list of gene names
@@ -149,7 +198,7 @@ tell_me_about_yourself <- function(genes){
   all.df <- pblapply(genes, function(g) {
     r_search <- entrez_search(db="gene", term=g)
     if(length(r_search$ids) > 0){
-      info <- entrez_summary(db="gene", id=r_search$ids)
+      info <- entrez_summary(db="gene", id=r_search$ids, version="2.0")
       df = data.frame("Gene"=g, 
                       "name"=extract_from_esummary(info, "name"),
                       "description"=extract_from_esummary(info, "description"), 
