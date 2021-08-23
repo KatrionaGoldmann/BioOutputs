2367311665a967071b5719409041e9844b4844d5
Katriona Goldmann <k.goldmann@qmul.ac.uk>
2021-08-23 17:42:02 +0100

    allow outline in facet

--- R/bio_facet.R
+++ R/bio_facet.R
@@ -2,6 +2,8 @@
 #'
 #' @param text The facet title
 #' @param colour Facet background colour
+#' @param outline_colour Colour for facet outline (default="white")
+#' @param outline_width Size of face outline (default=5)
 #' @param fontcolour Font colour
 #' @param angle Text angle/direction (0=horizontal, 90=vertical)
 #' @param align Text alignment
@@ -18,13 +20,12 @@
 #' width=ggplot(iris, aes(x=Species, y=Sepal.Width)) + geom_boxplot()
 #' ggarrange(blank, col1, col2, row, length, width, ncol=3, heights=c(0.05, 1), widths=c(0.1, 1, 1))
 
-
-
-bio_facet = function(text, colour="grey80", fontcolour="black", angle=90, align="center", size=5){
+bio_facet = function(text, colour="grey80", outline_colour="white", outline_width=5,  
+                     font_colour="black", angle=90, align="center", size=5){
   p = ggplot(df=NULL)  + 
     theme_void() + 
-    annotate("text", x=0, y=1, label=text, angle=angle, colour=fontcolour, size=size) + 
-    theme(panel.background=element_rect(fill=colour, color=colour)) 
+    annotate("text", x=0, y=1, label=text, angle=angle, colour=font_colour, size=size) + 
+    theme(panel.background=element_rect(fill=colour, color=outline_colour, size=outline_width))
   if(align == "left") p = p + lims(x=c(0, 1))
   if(align == "right") p = p + lims(x=c(-1, 0))
   return(p)
